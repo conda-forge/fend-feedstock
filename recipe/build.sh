7a1fe6393890c84ef7b82b9699162f5d886b6030
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-01 20:38:07 -0800

    enable rattler-build

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,9 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+export CARGO_PROFILE_RELEASE_STRIP=symbols
+export CARGO_PROFILE_RELEASE_LTO=fat
+
 # check licenses
 cargo-bundle-licenses \
     --format yaml \
@@ -12,6 +15,3 @@ cargo install --bins --no-track --locked --root ${PREFIX} --path cli
 ./documentation/build.sh
 mkdir -p ${PREFIX}/share/man/man1
 install -m 644 documentation/fend.1 ${PREFIX}/share/man/man1/fend.1
-
-# strip debug symbols
-"$STRIP" "$PREFIX/bin/${PKG_NAME}"
